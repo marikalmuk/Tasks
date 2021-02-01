d629d1a6b1b4d0694bb0ca8ee895b63ed1d869f0
Mari Kalmuk <mariakalmuk@gmail.com>
2021-02-01 14:59:49 +0200

    #3 - hipotenuse and right triangle area calculations

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Cathetus1");
+            var cat1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Cathetus2");
+            var cat2 = Convert.ToDouble(Console.ReadLine());
+
+            var hip = Math.Sqrt(Math.Pow(cat1, 2) + Math.Pow(cat2, 2));
+            Console.WriteLine($"The hipothenuse is {hip}");
+
+            var area = 0.5 * cat1 * cat2;
+            Console.WriteLine($"The area is {area}");
+        }
+    }
+}
