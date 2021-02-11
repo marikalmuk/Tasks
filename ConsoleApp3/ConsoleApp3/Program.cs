04b1f168bcc4e9f33c7cd5072c23cc081e475887
Mari Kalmuk <mariakalmuk@gmail.com>
2021-02-11 16:49:53 +0200

    #6 - added non-formated multiplication table for any range of nunbers

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Start number:");
+            int start_num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter End number:");
+            int end_num = Convert.ToInt32(Console.ReadLine());
+
+            for (int header = start_num; header <= end_num; header++)
+            {
+                Console.WriteLine($"Header is: {header}");
+
+                for (int header2 = start_num; header2 <= end_num; header2++)
+                {
+                    Console.WriteLine($"{header * header2}");
+                }
+            }
+
+
+        }
+    }
+}
